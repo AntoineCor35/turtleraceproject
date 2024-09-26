7a877e600574410cc619e41c5c9a64f9ae1e16c8
Singuila01 <mbayechris201@gmail.com>
2024-09-26 16:47:31 +0200

    modification du CSS pour le Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,30 @@
 import { Link } from 'react-router-dom';
 import logo from '../styles/visuals/logo.png';
+import '../styles/Header.css'
 
 const Header = ({ isAuthenticated, onLogout }) => {
     
     return (
     <header>
       {/* Le logo renvoie vers la racine ("/") */}
-      <Link to="/">
-        <img src={logo} height={100} alt="Logo" />
-      </Link>
+        <div className='header'>
+          <Link to="/">
+            <img className='logo' src={logo} height={100} alt="Logo" />
+          </Link>
+            {isAuthenticated && (
+            <>
+              {/* <Link to="/account" className="btn">Mon Compte</Link> Lien vers la page de compte */}
+              <Link to="/account" className='link'>
+                <button className='btn mon_compte'>Mon Compte</button>
+              </Link> {/* Lien vers la page de compte */}
+              <button className="btn logout" onClick={onLogout}>
+                Se déconnecter
+              </button>
+            </>
+          )}
+        </div>
       
-      {isAuthenticated && (
-        <>
-          <Link to="/account" className="btn">Mon Compte</Link> {/* Lien vers la page de compte */}
-          <button className="logout" onClick={onLogout}>
-            Se déconnecter
-          </button>
-        </>
-      )}
+      
     </header>
   );
 };
