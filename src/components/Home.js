6dc31fcacef9e2f8cd25dba32793b282ab409096
Antoine Cormier <antoinecormier@MacBook-Pro-de-Antoine.local>
2024-09-26 10:23:17 +0200

    ajout d'authentification

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,32 @@
 import QuizzApp from "./QuizzApp";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SignIn from "./SignIn"; // Import du composant SignIn
 import Register from "./Register"; // Import du composant Register
 import '../styles/Home.css';
 
 function Home() {
+  // Récupère l'utilisateur dans le localStorage
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
   const [isAuthenticated, setIsAuthenticated] = useState(false); // État pour suivre l'authentification
   const [showSignIn, setShowSignIn] = useState(true); // État pour basculer entre SignIn et Register
 
-  // Simule la connexion de l'utilisateur
-  const handleLogin = () => {
-    setIsAuthenticated(true);
+  useEffect(() => {
+    // Vérifie si l'utilisateur est authentifié
+    if (user && user.status === "success") {
+      setIsAuthenticated(true); // Si le statut est "success", on considère que l'utilisateur est connecté
+    }
+  }, [user]); // L'effet se déclenche à chaque modification de l'utilisateur
+
+  const handleUserState = (newUser) => {
+    setUser(newUser);
+    localStorage.setItem('user', JSON.stringify(newUser)); // Sauvegarde dans le localStorage
   };
 
   // Simule la déconnexion de l'utilisateur
   const handleLogout = () => {
     setIsAuthenticated(false);
+    localStorage.removeItem('user'); // Supprime l'utilisateur du localStorage à la déconnexion
+    setUser(null); // Réinitialise l'état user
   };
 
   // Fonction pour basculer entre SignIn et Register
@@ -34,11 +45,17 @@ function Home() {
             {showSignIn ? "Créer un compte" : "Se connecter"}
           </button>
 
-          <button className="login" onClick={handleLogin}>
-            {showSignIn ? "Se connecter" : "Créer un compte"} {/* Action principale */}
-          </button>
-          
-          {showSignIn ? <SignIn /> : <Register />} {/* Affiche SignIn ou Register */}
+          {showSignIn ? (
+            <button className="login" onClick={() => handleUserState({ status: "success" })}>
+              Se connecter
+            </button>
+          ) : (
+            <button className="login" onClick={() => handleUserState({ status: "success" })}>
+              Créer un compte
+            </button>
+          )}
+
+          {showSignIn ? <SignIn userState={handleUserState} /> : <Register userState={handleUserState} />} {/* Affiche SignIn ou Register */}
 
         </div>
       ) : (
